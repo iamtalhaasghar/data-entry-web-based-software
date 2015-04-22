ad96142350181514cc570ca29252fb05c0f3af1e
Juan José Denis Corrales <jjdenis@gmail.com>
2015-04-22 14:29:00 +0200

    Add bootstrap css format

--- crudapp/controller/control.py
+++ crudapp/controller/control.py
@@ -5,54 +5,43 @@ from crudapp import app
 
 import flask
 
-import database
-from models import Person
-import curl
+import crudapp.model.model as model
 
-database.init_db()
-session = database.db_session
+import curl
 
 curl.add_curl(app)
 
 
 @app.teardown_appcontext
 def shutdown_session(exception=None):
-    session.remove()
+    model.session.remove()
 
 
 @app.route('/')
 def index():
-    q = Person.query.all()
+    q = model.all_people()
     return flask.render_template('index.html', people=q)
 
 
 @app.route('/new_person', methods=['POST'])
 def add_person():
-    np = Person(firstname=flask.request.form['firstname'],
-                lastname=flask.request.form['lastname'])
-    session.add(np)
-    session.commit()
+    model.add_person(firstname=flask.request.form['firstname'],
+                     lastname=flask.request.form['lastname'])
     return flask.redirect(flask.url_for('index'))
 
 
 @app.route('/delete_person', methods=['POST'])
 def delete_person():
-    p = session.query(Person).filter_by(
-        person_id=flask.request.form['person_id']
-    ).one()
-    session.delete(p)
-    session.commit()
+    model.delete_person(person_id=flask.request.form['person_id'])
+
     return flask.redirect(flask.url_for('index'))
 
 
 @app.route('/edit_person', methods=['POST'])
 def edit_person():
-    p = session.query(Person).filter_by(
-        person_id=flask.request.form['person_id']
-    ).one()
-    p.lastname = flask.request.form['lastname']
-    p.firstname = flask.request.form['firstname']
-    session.commit()
+    model.edit_person(person_id=flask.request.form['person_id'],
+                      firstname=flask.request.form['firstname'],
+                      lastname=flask.request.form['lastname'])
     return flask.redirect(flask.url_for('index'))
 
 
