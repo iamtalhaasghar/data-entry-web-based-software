560ed202935e2fa2fc02da1fc2964c881428aa3c
Juan José Denis Corrales <jjdenis@gmail.com>
2015-04-22 10:04:14 +0200

    Reorganize as MVC

---
+++ crudapp/controller/control.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from crudapp import app
+
+import flask
+
+import database
+from models import Person
+import curl
+
+database.init_db()
+session = database.db_session
+
+curl.add_curl(app)
+
+
+@app.teardown_appcontext
+def shutdown_session(exception=None):
+    session.remove()
+
+
+@app.route('/')
+def index():
+    q = Person.query.all()
+    return flask.render_template('index.html', people=q)
+
+
+@app.route('/new_person', methods=['POST'])
+def add_person():
+    np = Person(firstname=flask.request.form['firstname'],
+                lastname=flask.request.form['lastname'])
+    session.add(np)
+    session.commit()
+    return flask.redirect(flask.url_for('index'))
+
+
+@app.route('/delete_person', methods=['POST'])
+def delete_person():
+    p = session.query(Person).filter_by(
+        person_id=flask.request.form['person_id']
+    ).one()
+    session.delete(p)
+    session.commit()
+    return flask.redirect(flask.url_for('index'))
+
+
+@app.route('/edit_person', methods=['POST'])
+def edit_person():
+    p = session.query(Person).filter_by(
+        person_id=flask.request.form['person_id']
+    ).one()
+    p.lastname = flask.request.form['lastname']
+    p.firstname = flask.request.form['firstname']
+    session.commit()
+    return flask.redirect(flask.url_for('index'))
+
+
+# This adds curl-like logging
+
+if __name__ == '__main__':
+    app.run(debug=True)
